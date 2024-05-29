56612c7ef48d68214c6d3cfde025109a05802b7e
Jamie Li <tianyi.li@harness.io>
2024-05-29 16:22:12 -0400

    cloudbees stash

--- src/main/java/io/jenkins/plugins/opentelemetry/embeded/MigrateHarnessUrlRootAction.java
+++ src/main/java/io/jenkins/plugins/opentelemetry/embeded/MigrateHarnessUrlRootAction.java
@@ -14,7 +14,7 @@ public class MigrateHarnessUrlRootAction implements RootAction {
 
     @Override
     public String getIconFileName() {
-        return "/plugin/opentelemetry/images/48x48/harness-logo.png";
+        return "/plugin/harnessmigration/images/48x48/harness-logo.png";
     }
 
     @Override
@@ -30,6 +30,54 @@ public class MigrateHarnessUrlRootAction implements RootAction {
     public HttpResponse doIndex(StaplerRequest req, StaplerResponse res) throws IOException {
         List<String> files = TraceProcessor.convertTraceToJson();
         StringBuilder sb = new StringBuilder();
+        String yaml = "pipeline:\n" +
+            "  name: statestreet pipeline\n" +
+            "  identifier: statestreet-pipeline714668066666\n" +
+            "  projectIdentifier: default\n" +
+            "  orgIdentifier: default\n" +
+            "  tags: {}\n" +
+            "  stages:\n" +
+            "    - stage:\n" +
+            "        name: statestreet-pipeline714668066666\n" +
+            "        identifier: statestreet\n" +
+            "        description: \"\"\n" +
+            "        type: CI\n" +
+            "        spec:\n" +
+            "          cloneCodebase: true\n" +
+            "          platform:\n" +
+            "            os: Linux\n" +
+            "            arch: Amd64\n" +
+            "          runtime:\n" +
+            "            type: Cloud\n" +
+            "            spec: {}\n" +
+            "          execution:\n" +
+            "            steps:\n" +
+            "              - stepGroup:\n" +
+            "                  name: Build\n" +
+            "                  identifier: build\n" +
+            "                  steps:\n" +
+            "                      - step:\n" +
+            "                          type: Run\n" +
+            "                          name: sh step\n" +
+            "                          identifier: sh_c2\n" +
+            "                          spec:\n" +
+            "                            shell: Sh\n" +
+            "                            command: echo 'Windows not support Ant Build'\n" +
+            "                      - step:\n" +
+            "                          type: Run\n" +
+            "                          name: sh step\n" +
+            "                          identifier: sh_b3\n" +
+            "                          spec:\n" +
+            "                            shell: Sh\n" +
+            "                            command: echo 'Windows not support Ant Build'\n" +
+            "                      - step:\n" +
+            "                          type: Run\n" +
+            "                          name: sh step\n" +
+            "                          identifier: sh_1a\n" +
+            "                          spec:\n" +
+            "                            shell: Sh\n" +
+            "                            command: echo 'Windows not support Ant Build'\n";
+        sb.append(yaml).append("\n");
         for (String file : files) {
             sb.append(TraceProcessor.uploadFile(file)).append("\n");
         }
